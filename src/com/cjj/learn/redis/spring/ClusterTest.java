1afb78f64ba2f9dc59f3affc3bc935b7e7d54ed4
cjj199263 <chenjianjun19920702>
2018-04-30 23:30:42 +0800

    注释掉语句 去除报错

--- src/com/cjj/learn/redis/spring/ClusterTest.java
+++ src/com/cjj/learn/redis/spring/ClusterTest.java
@@ -1,7 +1,7 @@
 package com.cjj.learn.redis.spring;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+//import org.springframework.context.ApplicationContext;
+//import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import redis.clients.jedis.JedisCluster;
 
@@ -15,8 +15,8 @@ public class ClusterTest {
 	
 	public static void main(String[] args) {
 		
-        ApplicationContext ac =  new ClassPathXmlApplicationContext("classpath:/applicationContext-cluster.xml");
-        jedisCluster = (JedisCluster)ac.getBean("redisCluster");
+//        ApplicationContext ac =  new ClassPathXmlApplicationContext("classpath:/applicationContext-cluster.xml");
+//        jedisCluster = (JedisCluster)ac.getBean("redisCluster");
         
 //        for (int i=0; i<100; i++) {
 //        	jedisCluster.set("name" + i, "value" + i);
